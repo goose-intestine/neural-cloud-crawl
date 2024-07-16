4632e589a443c63e6d8fbe9447e4013adc012262
Jerry Or <jerryor@ccn-technologies.com>
2024-07-16 10:17:03 +0800

    Prepare for gh-pages

--- defaultValues.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-const DefaultValues = {
-  chromePath: process.env.CHROME_PATH,
-  profilePath: process.env.PROFILE_PATH,
-  profileName: process.env.PROFILE_NAME,
-};
-
-export { DefaultValues };
--- test.js
+++
@@ -1,3 +0,0 @@
-import { download } from "./parent-download.js";
-
-await download("表情包");
